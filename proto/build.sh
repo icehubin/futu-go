5adff194724461d63d73ecafae8f0e8e5cd91f7d
icehu <icehu@vip.qq.com>
2021-09-25 14:23:54 +0800

    proto fix package

--- proto/build.sh
+++ proto/build.sh
@@ -2,5 +2,8 @@
 
 #直接引用的 https://github.com/FutunnOpen/py-futu-api/tree/master/futu/common/pb
 
-sed -i "" "s:github.com/futuopen/ftapi4go/::g" `ls *.proto`
+sed -i "" "s:github.com/futuopen/ftapi4go/:github.com/icehubin/futu-go/:g" `ls *.proto`
 ls *.proto |awk '{print "protoc -I=./ --go_out=../ ./"$1}'|sh
+sed -i "" "s:github.com/icehubin/futu-go/:github.com/futuopen/ftapi4go/:g" `ls *.proto`
+cp -rf ../github.com/icehubin/futu-go/pb/* ../pb
+rm -rf ../github.com
